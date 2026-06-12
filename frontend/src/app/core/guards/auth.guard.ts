1f3f3ac4547e26bc620979bf1c9c5428f1ca78d5
Gabriel-Arantes-git <arantes.gabriel21@gmail.com>
2026-06-12 12:04:52 -0300

    fix(auth.guard e index.html): corrigindo erros resportados pelo SonarQube

--- frontend/src/app/core/guards/auth.guard.ts
+++ frontend/src/app/core/guards/auth.guard.ts
@@ -1,6 +1,5 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
-import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
 export const authGuard: CanActivateFn = () => {
